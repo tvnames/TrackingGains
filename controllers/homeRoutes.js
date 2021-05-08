2c3cd083a135c276b1726beeda3b936ce9fcffab
Andrew Parsons <77991510+tvnames@users.noreply.github.com>
2021-05-07 21:05:01 -0700

    Fixed route paths

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
 const path = require("path");
 
-router.get("/", function (req, res) {
+router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-router.get("/exercise", function (req, res) {
+router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-router.get("/stats", function (req, res) {
+router.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
